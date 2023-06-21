fe3f1f6733066fdfc9a576413d7811aa9aa2e13b
Santiago Millan <104397283+SantiagoNivelics@users.noreply.github.com>
2023-06-20 23:52:14 -0500

    .

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 const API = "https://api.escuelajs.co/api/v1"
 
 async function fetchDta(urlApi) {
-    const response = await fetch(urlApi)
+    const response = await fetch(urlApi) 
     const data = await response.json()
     return data
 }
